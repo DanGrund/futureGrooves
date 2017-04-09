3d1fa46fa9d205e17f1138839e67a34eb9fbb391
mlimberg <limbergmike@gmail.com>
2017-04-09 17:22:23 -0600

    Add ability to post new user to DB

--- lib/components/CreateNewUser/index.jsx
+++ lib/components/CreateNewUser/index.jsx
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import './new-user-styles.scss'
+import UserContainer from '../../containers/UserContainer'
 
 export class CreateNewUser extends Component {
   constructor() {
     super()
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+    }
   }
+
+  submitNewUser() {
+    const { username, email, password } = this.state
+    if(username && email && password) {
+      this.props.postNewUser({ username, email, password })
+    }
+  }
+
 
   render() {
     return (
       <div className='new-user-form-container'>
         <h1>New User!</h1>
-        <input />
+        <div className='new-user-form'>
+          <input placeholder='username'
+                 className='new-user-input'
+                 onChange={(e) => this.setState({ username: e.target.value })}/>
+          <input placeholder='email'
+                 className='new-user-input'
+                 onChange={(e) => this.setState({ email: e.target.value})}/>
+          <input placeholder='password'
+                 className='new-user-input'
+                 onChange={(e) => this.setState({ password: e.target.value})}/>
+          <button className='new-user-submit'
+                  onClick={this.submitNewUser.bind(this)}>
+            Submit
+          </button>
+        </div>
       </div>
     )
   }
 }
 
-export default CreateNewUser
+export default UserContainer(CreateNewUser)
--- lib/containers/UserContainer.jsx
+++ lib/containers/UserContainer.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchUser } from '../actions'
+import { fetchUser, postNewUser } from '../actions'
 import UserProfile from '../components/UserProfile'
 
 const mapDispatchToProps = {
   fetchUser,
+  postNewUser
 }
 
 const mapStateToProps = (state) => {
   return {
-    user: state.UserReducer.user,
+    user: state.ActiveUser.username,
   }
 }
 
