7438574d69d78efe377c915e3cd02a3a9dc7cf30
mlimberg <limbergmike@gmail.com>
2017-04-09 23:15:01 -0600

    Add functionality to logout removes fields on sign in removes user from local storage

--- lib/components/CreateNewUser/index.jsx
+++ lib/components/CreateNewUser/index.jsx
@@ -16,6 +16,7 @@ export class CreateNewUser extends Component {
     const { username, email, password } = this.state
     if(username && email && password) {
       this.props.postNewUser({ username, email, password })
+      this.setState({ username: '', email: '', password: ''})
     }
   }
 
@@ -38,12 +39,16 @@ export class CreateNewUser extends Component {
         <div className='new-user-form'>
           <input placeholder='username'
                  className='new-user-input'
+                 value={username}
                  onChange={(e) => this.setState({ username: e.target.value })}/>
           <input placeholder='email'
                  className='new-user-input'
+                 value={email}
                  onChange={(e) => this.setState({ email: e.target.value})}/>
           <input placeholder='password'
                  className='new-user-input'
+                 type='password'
+                 value={password}
                  onChange={(e) => this.setState({ password: e.target.value})}/>
           <button className='new-user-submit'
                   disabled={!username || !email || !password}
