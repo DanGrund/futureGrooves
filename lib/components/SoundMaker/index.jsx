6d12bd1c84ae40ccd6151898e9e6cc4a90b43f08
Dylan Avery <fossilfuelmusic@gmail.com>
2017-04-15 21:00:02 -0600

    Switch editing of saved sound over to Patch

--- lib/components/SoundMaker/index.jsx
+++ lib/components/SoundMaker/index.jsx
@@ -86,14 +86,19 @@ export class SoundMaker extends Component {
 
   saveSound = () => {
     let soundName = prompt('What do you want to call your sound')
-    this.setState(update(this.state, { spec: { soundName: { $set: soundName } } }), () => this.props.saveSound(JSON.stringify(this.state.spec), this.props.user.id))
+    if(!this.props.sound.editsound) {
+      this.setState(update(this.state, { spec: { soundName: { $set: soundName } } }), () => this.props.saveSound(JSON.stringify(this.state.spec), this.props.user.id, "POST"))
+    } else {
+      this.setState(update(this.state, { spec: { soundName: { $set: soundName } } }), () => this.props.saveSound(JSON.stringify(this.state.spec), this.props.user.id,'PATCH', this.state.id))
+    }
     this.setState({ savedchanges: true })
   }
 
   loadSound = () => {
     let soundID = prompt('Enter the ID of the sound you want to edit')
     this.props.loadSound(soundID)
-    this.setState({spec: this.props.sound.editsound})
+    console.log(this.props.sound)
+    this.setState({spec: this.props.sound.editsound, id: this.props.sound.id})
   }
 
   updateValue = (key) => ({ target }) => {
