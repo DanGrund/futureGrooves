3583b5aee97116c096bb5ca76bee993b3c9c90e0
Dylan Avery <fossilfuelmusic@gmail.com>
2017-04-18 15:13:48 -0600

    Branch change

--- lib/components/Home/index.jsx
+++ lib/components/Home/index.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import IndividualSong from '../IndividualSong/IndividualSong'
 
-const Home = () => {
-  return(
-    <div>
-      <ul className='stream'>
-        <li>Stream 1</li>
-        <li>Stream 2</li>
-        <li>Stream 3</li>
-      </ul>
-    </div>
-  )
+export class Home extends Component {
+  constructor(){
+    super()
+    this.state ={
+
+    }
+  }
+
+  componentDidMount(){
+    
+  }
+
+  render() {
+    return(
+      <div>
+        <ul className='stream'>
+          <li>Stream 1</li>
+          <li>Stream 2</li>
+          <li>Stream 3</li>
+        </ul>
+      </div>
+      )
+    }
 }
 
 export default Home
